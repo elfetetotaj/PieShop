a5e5ae6d42de51f6f9a769f70f783025afb00b46
Elfete Totaj <et48436@ubt-uni.net>
2022-04-06 13:34:21 +0200

    Pie and category models and simply use mock data

---
+++ Controllers/PieController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+using PieShop.ViewModels;
+
+namespace PieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepozitory;
+        private readonly ICategoryRepository _categoryRepozitory;
+
+        public PieController(IPieRepository pieRepository,ICategoryRepository categoryRepository)
+        {
+            _pieRepozitory = pieRepository;
+            _categoryRepozitory = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepozitory.AllPies;
+
+            piesListViewModel.CurrentCategory = "Chesse cake";
+            return View(piesListViewModel);
+        }
+    }
+}
---
+++ Models/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace PieShop.Models
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> AllCategories { get; }
+
+    }
+}
